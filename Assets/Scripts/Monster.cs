ae4d0376e026743c5bed29abaac9b76af790a619
UchidaShunsuke <uchishun04@yahoo.co.jp>
2025-04-20 02:00:33 +0900

    GameScore

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,15 +6,68 @@ using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
+    public Player player;   //プレイヤーの情報
+    private int levelMinHp = 20;    //最低レベルのHP
+    private int levelMaxHp = 10000; //最大レベルのHP
+    private int maxLevel = 100;     //最大レベル
+    private float degree = 1.2f;    //指数
+    private float coinMultiplier = 0.5f;    //コインの獲得倍率
     public TMP_Text hpLabel;    //HPテキスト
     private int hp;         //HP
     private int maxHp;      //最大HP
+    public Image monsterImage;  //画像を切り替えるコンポーネント
+    public Sprite[] monsterImages;  //モンスターのリスト
+
+    //モンスターのHPを計算する
+    private int CalcHp()
+    {
+        //モンスターを倒した数に応じて何割のHPにするか
+        float tmp = Mathf.Pow((float)player.kill / maxLevel, degree);
+
+        //式に合わせてHPを決定
+        int hp = (int)((levelMaxHp - levelMinHp) * tmp + levelMinHp + 0.5f);
+
+        return hp;
+    }
+
+    
+    //モンスターの初期化
+    private void Setup()
+    {
+        maxHp = CalcHp();
+        hp = maxHp;
+        hpLabel.text = hp + "/" + maxHp;
+
+        //どの画像を使うか乱数で決める
+        int imageIndex = Random.Range(0, monsterImages.Length);
+
+        //画像の変更
+        monsterImage.sprite = monsterImages[imageIndex];
+    }
 
     void Start()
     {
-        maxHp = 10;
-        hp = maxHp;
+        Setup();
+    }
+
+    public void OnClickMonster()
+    {
+        //ダメージを与える
+        hp -= player.level;
         hpLabel.text = hp + "/" + maxHp;
+
+        //もし死んでいたら
+        if(hp <= 0)
+        {
+            //プレイヤーの倒したモンスターの数を増やす
+            player.kill++;
+            //HPからコインの数を計算
+            int amount = (int)(CalcHp() * coinMultiplier);
+            //コインを追加
+            player.AddCoin(amount);
+            //次のモンスターを出す
+            Setup();
+        }
     }
 
     void Update()
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public TMP_Text coinLabel;  //コインテキスト
+    public TMP_Text levelLabel; //レベルテキスト
+    public int coin;    //現在の所持コイン
+    public int level;   //現在のレベル
+    public int kill;    //敵を倒した数
+    public Button powerUpBtn;   //パワーアップボタン
+    public TMP_Text powerUpCoinText;    //パワーアップに必要なコインの数
+
+    //パワーアップに必要なコインを計算
+    public int PowerUpCoin()
+    {
+        return (level - 1) * 20 + 100;
+    }
+
+    public void UpdateUI()
+    {
+        //コインのテキスト
+        coinLabel.text = "Coin: " + coin;
+
+        //現在のレベル
+        levelLabel.text = "Level: " + level;
+
+        //パワーアップボタン
+        int require = PowerUpCoin();
+        if(coin < require)
+        {
+            powerUpBtn.interactable = false;
+        }
+        else
+        {
+            powerUpBtn.interactable = true;
+        }
+
+        //パワーアップに必要なコインのテキスト
+        powerUpCoinText.text = "Coin: " + require;
+    }
+
+    public void AddCoin(int amount)
+    {
+        coin += amount;
+        UpdateUI();
+    }
+
+    public void OnPowerUp()
+    {
+        int require = PowerUpCoin();
+        if(coin >= require)
+        {
+            level++;
+            AddCoin(-require);
+        }
+    }
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    void Update()
+    {
+        
+    }
+}
